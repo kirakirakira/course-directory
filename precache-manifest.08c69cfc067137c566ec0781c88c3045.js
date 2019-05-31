23fc9102da9fd0cde74f50c7752d294a5c56eaeb
Kira Hartlage <kira.hartlage@gmail.com>
2019-05-31 14:36:26 -0400

    Updates

---
+++ precache-manifest.08c69cfc067137c566ec0781c88c3045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "e07d78dd3f399d85a886",
+    "url": "/course-directory/static/js/main.e07d78dd.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "e07d78dd3f399d85a886",
+    "url": "/course-directory/static/css/main.e52446d7.chunk.css"
+  },
+  {
+    "revision": "4622660881154df6104e052c9a311827",
+    "url": "/course-directory/index.html"
+  }
+];
